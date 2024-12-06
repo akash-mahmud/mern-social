50ceff11832a7469df377b11b573e95951044867
Akash <mahmudakash177@gmail.com>
2024-12-07 03:59:07 +0600

    changes database url

--- user-functions/function/config/config.js
+++ user-functions/function/config/config.js
@@ -1,15 +1,16 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 const config = {
-  env: process.env.NODE_ENV || 'development',
+  env: process.env.NODE_ENV || "development",
   port: process.env.PORT || 8000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri:"mongodb+srv://akash:1234@cluster0.avud8.mongodb.net/mernproject?retryWrites=true&w=majority&appName=Cluster0"
+  mongoUri:
+    "mongodb+srv://abdallahimadhassan:V1D6MBxSW3NxqUyf@openfaas.xq2dc.mongodb.net/?retryWrites=true&w=majority&appName=OpenFaaS",
   //  process.env.MONGODB_URI ||
   //   process.env.MONGO_HOST ||
   //   'mongodb://' + (process.env.IP || 'localhost') + ':' +
-  //   (process.env.MONGO_PORT || '27017') 
+  //   (process.env.MONGO_PORT || '27017')
   //   +
   //   '/mernproject'
 };
